f4d25bb9266a94b87ed1330f179fcd96cb57ebc7
yougotmyphone <110523886+prgrmr-l@users.noreply.github.com>
2023-12-09 00:21:19 +0900

    linewidth_range_startend_arc

---
+++ app.js
@@ -0,0 +1,39 @@
+const lineWidth = document.getElementById("line-width");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function onClick(event) {
+  ctx.arc(event.offsetX, event.offsetY, lineWidth.value / 100, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+function startPainting(event) {
+  ctx.arc(event.offsetX, event.offsetY, lineWidth.value / 100, 0, 2 * Math.PI);
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+lineWidth.addEventListener("change", onLineWidthChange);
