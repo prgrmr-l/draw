103cfaa567dd65c618283229a4ed08fe32748f9c
yougotmyphone <110523886+prgrmr-l@users.noreply.github.com>
2023-12-09 21:18:02 +0900

    fillRect_mdf_onCanvasClick

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
@@ -9,6 +10,7 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = true;
 
 function onMove(event) {
   if (isPainting) {
@@ -18,9 +20,28 @@ function onMove(event) {
   }
   ctx.moveTo(event.offsetX, event.offsetY);
 }
-function onClick(event) {
-  ctx.arc(event.offsetX, event.offsetY, lineWidth.value / 100, 0, 2 * Math.PI);
-  ctx.stroke();
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Draw";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "fill";
+  }
+}
+function onCanvasClick(event) {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  } else {
+    ctx.arc(
+      event.offsetX,
+      event.offsetY,
+      lineWidth.value / 100,
+      0,
+      2 * Math.PI
+    );
+    ctx.stroke();
+  }
 }
 function startPainting(event) {
   ctx.arc(event.offsetX, event.offsetY, lineWidth.value / 100, 0, 2 * Math.PI);
@@ -47,7 +68,7 @@ function onColorClick(event) {
   colors.value = colorValue;
 }
 
-canvas.addEventListener("click", onClick);
+canvas.addEventListener("click", onCanvasClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -58,8 +79,4 @@ colors.addEventListener("change", onColorChange);
 colorOptions.forEach((colors) =>
   colors.addEventListener("click", onColorClick)
 );
-//colors는 html에 있는 "색깔 input"에서 색깔을 선택하고 발생하는 event에서 target.value의 값을 가지고 온다.
-//colorOptions의 각각의 색깔은 colors.addEventListener("click", onColorClick)의 결과에서 나온 값을 매개로 colors와 연동한다.
-//onColorClick을 변수로하는 함수에서는 배열 colorOptions에 속한 각 color-option 클래스를 선택할 때마다 발생하는 target.dataset.color을 참고한다.
-//참고한 target.dataset.color는 colors와 매개가 되어 colors.addEventListener("change", onColorChange에 적용
-// ctx.strokeStyle = event.target.value와 ctx.fillStyle = event.target.value를 통해 색을 변화한다.
+modeBtn.addEventListener("click", onModeClick);
